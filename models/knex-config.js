b2a806110fab79229b91c296cee417e520570007
MJP <markangelpenaranda@gmail.com>
2016-11-22 13:57:42 +0800

    knex

--- models/knex-config.js
+++ models/knex-config.js
@@ -1,7 +1,8 @@
 var knex = require('knex')({
   client: 'pg',
-  connection: process.env.PG_CONNECTION_STRING,
-  searchPath: 'knex,public'
+  connection: "postgres://zlsgbnkwicjyfw:YbUdvA2O6B9QzDatx_jQ0qVDv4@ec2-54-243-249-37.compute-1.amazonaws.com:5432/d8612r0fl89fh",
+  searchPath: 'knex,public',
+  ssl: true
 });
 
 module.exports = knex;

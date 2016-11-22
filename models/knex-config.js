ce13b18f317d5389106c0fa4342d8d1cda36e472
MJP <markangelpenaranda@gmail.com>
2016-11-22 14:14:33 +0800

    knex

--- models/knex-config.js
+++ models/knex-config.js
@@ -1,6 +1,6 @@
 var knex = require('knex')({
   client: 'pg',
-  connection: process.env.PG_CONNECTION_STRING,
+  connection: "postgres://zlsgbnkwicjyfw:YbUdvA2O6B9QzDatx_jQ0qVDv4@ec2-54-243-249-37.compute-1.amazonaws.com:5432/d8612r0fl89fh",
   searchPath: 'knex,public'
 });
 

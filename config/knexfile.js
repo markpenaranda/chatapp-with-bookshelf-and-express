c4fda6e7bc8beb1534d9364c795f104385e1a97c
MJP <markangelpenaranda@gmail.com>
2016-11-22 14:21:45 +0800

    dev

--- config/knexfile.js
+++ config/knexfile.js
@@ -1,16 +1,9 @@
 module.exports = {
-    production: {
-	client: 'postgresql',
-	connection: {
-		database: 'postgres://zlsgbnkwicjyfw:YbUdvA2O6B9QzDatx_jQ0qVDv4@ec2-54-243-249-37.compute-1.amazonaws.com:5432/d8612r0fl89fh'
-	},
-  ssl: true	
-  },
+
   development: {
     client: 'postgresql',
     connection: {
-      database: 'postgres://zlsgbnkwicjyfw:YbUdvA2O6B9QzDatx_jQ0qVDv4@ec2-54-243-249-37.compute-1.amazonaws.com:5432/d8612r0fl89fh'
+      database: 'chatapp_dev'
     }
-  },
-  ssl: true
+  }
 }
--- models/knex-config.js
+++ models/knex-config.js
@@ -1,8 +1,7 @@
 var knex = require('knex')({
   client: 'pg',
-  connection: "postgres://zlsgbnkwicjyfw:YbUdvA2O6B9QzDatx_jQ0qVDv4@ec2-54-243-249-37.compute-1.amazonaws.com:5432/d8612r0fl89fh",
-  searchPath: 'knex,public',
-  ssl: true
+  connection: process.env.PG_CONNECTION_STRING,
+  searchPath: 'knex,public'
 });
 
 module.exports = knex;
